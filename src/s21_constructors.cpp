a072bc2a12f528986c9618a75975f41b17d08fa8
Oleg Kuzmin <manwoodt@student.21-school.com>
2024-01-31 16:08:17 +0300

    change matrix_ from p** to p*

--- src/s21_constructors.cpp
+++ src/s21_constructors.cpp
@@ -5,36 +5,28 @@ S21Matrix::S21Matrix() : rows_(0), cols_(0), matrix_(nullptr){};
 // Параметризированный конструктор с количеством строк и столбцов
 S21Matrix::S21Matrix(int rows, int cols) : rows_(rows), cols_(cols) {
   // Выделение памяти для матрицы
-  matrix_ = new double*[rows_];
-  for (int i = 0; i < rows_; ++i) {
-    matrix_[i] = new double[cols_];
-
-
-    // Заполняем матрицу нулями
-    for (int j = 0; j < cols_; ++j) {
-      matrix_[i][j] = 0.0;
-    }
-  }
+  matrix_ = new double[rows_ * cols_];
 };
 
-
-S21Matrix::S21Matrix(const S21Matrix &other) : rows_(other.rows_), cols_(other.cols_){
-
-  matrix_ = new double*[rows_];
-  for (int i = 0; i < rows_; ++i) {
-    matrix_[i] = new double[cols_];
-  }
+// почему здесь тоже не можем matrix_(other.matrix_)
+S21Matrix::S21Matrix(const S21Matrix &other)
+    : rows_(other.rows_),
+      cols_(other.cols_),
+      matrix_(new double[rows_ * cols_]) {
+  std::memcpy(matrix_, other.matrix_,
+              other.rows_ * other.cols_ * sizeof(double));
 }
 
-/*
-S21Matrix(S21Matrix &&other);S
-*/
+S21Matrix::S21Matrix(S21Matrix &&other) noexcept
+    : rows_(other.rows_), cols_(other.cols_), matrix_(other.matrix_) {
+  other.matrix_ = nullptr;
+  other.rows_ = 0;
+  other.cols_ = 0;
+}
+
 S21Matrix::~S21Matrix() {
   // Освобождение памяти, выделенной для матрицы
-  if (matrix_ != nullptr) {
-    for (int i = 0; i < rows_; ++i) {
-      delete[] matrix_[i];
-    }
-    delete[] matrix_;
-  }
+  if (matrix_ != nullptr) delete[] matrix_;
+  rows_ = 0;
+  cols_ = 0;
 }
